af4dd0a553bb5a4814520907dc174b43874712b2
nick <nick.schmidt1711@gmail.com>
2025-06-06 10:02:53 +0200

    Update config_init.sh

--- .dotfiles/scripts/config_init.sh
+++ .dotfiles/scripts/config_init.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 git clone --bare git@github.com:shizeNick/.dotfiles.git $HOME/.dotfiles 
 # define config alias locally since the dotfiles
 # aren't installed on the system yet
