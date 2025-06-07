8bb06f05732d47625afbf60df5e173684ae2599a
nick <nick.schmidt1711@gmail.com_home>
2025-06-07 13:06:26 +0200

    weather block fix

--- .scripts/weather.sh
+++ .scripts/weather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+location=$(curl)
 firstry=$(curl wttr.in | grep -m 1 "°C")
-echo ${firstry} | sed 's/( )\.//' |sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"
-
-# sed (2) does delte the color(s) pattern
+echo ${firstry} | sed 's/( )\.//' | sed 's/,\\_//' | sed 's/_ \/""//' | sed 's/\.-\. //' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"
+# sed 1-3 for cancel the cloud and sun markdown
+# sed (4) does delte the color(s) pattern
