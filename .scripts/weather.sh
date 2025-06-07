2dba1e855c85f466524b4238c50148c763e724f1
nick <nick.schmidt1711@gmail.com_home>
2025-06-07 15:27:19 +0200

    weather location

--- .scripts/weather.sh
+++ .scripts/weather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-location=$(curl)
-firstry=$(curl wttr.in | grep -m 1 "°C")
+location=$(curl ifconfig.co/city)
+firstry=$(curl wttr.in/${location} | grep -m 1 "°C")
 echo ${firstry} | sed 's/( )\.//' | sed 's/,\\_//' | sed 's/_ \/""//' | sed 's/\.-\. //' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"
 # sed 1-3 for cancel the cloud and sun markdown
 # sed (4) does delte the color(s) pattern
