a191c8ef81ffe732b641769673f00c8316724b3b
rainvi <nick.schmidt1711@gmail.com_Laptop>
2025-06-26 16:37:53 +0200

    kitty theme and nvim theme light

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,23 +4,23 @@ return {
     --    vim.cmd.colorscheme("silverhand")
     --   
     --end,
-    { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000,
-    config = function ()
-        vim.cmd.colorscheme("moonfly")
-       
-    end, 
-    },
-    --"zenbones-theme/zenbones.nvim", -- if changing colorscheme, 
-    --                              -- dont forget to change or comment the config
-    --config = function()
-    --    vim.opt.termguicolors = true
-    --    vim.opt.background = "dark"
-    --    --vim.g.zenbones = {
-    --    --    lightness = "warm"
-    --    --}
-    --    vim.cmd.colorscheme("zenbones")
-    --end,
-    --dependencies = {
-    --    "rktjmp/lush.nvim",
-    --}
+    --{ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000,
+    --config = function ()
+    --    vim.cmd.colorscheme("moonfly")
+    --   
+    --end, 
+    --},
+    "zenbones-theme/zenbones.nvim", -- if changing colorscheme, 
+                                  -- dont forget to change or comment the config
+    config = function()
+        vim.opt.termguicolors = true
+        vim.opt.background = "light"
+        --vim.g.zenbones = {
+        --    lightness = "warm"
+        --}
+        vim.cmd.colorscheme("zenbones")
+    end,
+    dependencies = {
+        "rktjmp/lush.nvim",
+    }
 }
