611da31d4e85fc55317782b5042b2e92820481c0
rainvi <nick.schmidt1711@gmail.com_Laptop>
2025-07-10 20:02:24 +0200

    statusbar scripts & rebase

--- .scripts/weather.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-loc=$(cat ./location)
-firstry=$(curl wttr.in/${location} | grep -m 1 "°C")
-echo ${firstry} | sed 's/( )\.//' | sed 's/,\\_//' | sed 's/_ \/""//' | sed 's/\.-\. //' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"
-# sed 1-3 for cancel the cloud and sun markdown
-# sed (4) does delte the color(s) pattern
